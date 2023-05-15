e08436bd336304b5fb3f6d9b63345678cbb5168e
John 'Snow' Close <SnowdramaGames@gmail.com>
2023-05-14 23:52:57 -0400

    Added Color Palette, AutoHook, and CellularAutomata

---
+++ Core/ColorPalette/ColorPalette.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPalette : ScriptableObject
+{
+    [SerializeField]
+    private List<Color> _colors;
+    public List<Color> Colors
+    {
+        get { return _colors; }
+    }
+#if UNITY_EDITOR
+    public void AssignColors(List<Color> colors)
+    {
+        _colors = new List<Color>(colors);
+    }
+#endif
+}
