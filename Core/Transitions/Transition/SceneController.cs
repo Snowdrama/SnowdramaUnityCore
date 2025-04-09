9b001891f6e689064f9faac55c5ef824ac460c6a
John 'Snow' Close <SnowdramaGames@gmail.com>
2025-04-09 09:03:13 -0400

    Fix to remove editor scenemanagement from the transition that was added accidentally

--- Core/Transitions/Transition/SceneController.cs
+++ Core/Transitions/Transition/SceneController.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.LowLevel;
 using UnityEngine.SceneManagement;
 using UnityEngine.PlayerLoop;
 using Snowdrama.Core;
-using UnityEditor.SceneManagement;
 
 namespace Snowdrama.Transition
 {
