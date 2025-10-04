c9b3e36e617246ed82c07c648d1ed2ae73fd09a6
John 'Snowdrama <SnowdramaGames@gmail.com>
2025-10-03 21:07:20 -0400

    Added Square type to CellularAutomata shapes

--- Core/GameFeatures/CellularAutomata/ProcessingScripts/CellularAutomataProcessShape_Square.cs
+++ Core/GameFeatures/CellularAutomata/ProcessingScripts/CellularAutomataProcessShape_Square.cs
@@ -7,8 +7,8 @@ namespace Snowdrama.CellularAutomata
     public class CellularAutomataProcessShape_Square : CellularAutomataProcess
     {
         [SerializeField] private bool invert = false;
+        [SerializeField] private bool border = false;
         [SerializeField] private Vector2 size = Vector2.one;
-
         public override void Init() { }
 
         public override bool[,] Process(bool[,] data, int seed, ref int rngSequenceIndex)
@@ -18,18 +18,42 @@ namespace Snowdrama.CellularAutomata
             int squareOffsetX = Mathf.RoundToInt((data.GetLength(0) - squareWidth) * 0.5f);
             int squareOffsetY = Mathf.RoundToInt((data.GetLength(1) - squareHeight) * 0.5f);
 
-            for (int y = squareOffsetY; y < squareHeight; y++)
+            for (int y = 0; y < data.GetLength(1); y++)
             {
-                for (int x = squareOffsetX; x < squareWidth; x++)
+                for (int x = 0; x < data.GetLength(0); x++)
                 {
-                    if (invert)
+
+                    if (!border)
                     {
-                        data[x, y] = false;
+                        //if we're not border and inside the square
+                        if (x > squareOffsetX && x < squareOffsetX + squareWidth && y > squareOffsetY && y < squareOffsetY + squareHeight)
+                        {
+                            if (!invert)
+                            {
+                                data[x, y] = true;
+                            }
+                            else
+                            {
+                                data[x, y] = false;
+                            }
+                        }
                     }
                     else
                     {
-                        data[x, y] = true;
+                        //if we're border and not inside the square
+                        if (x < squareOffsetX || x > squareOffsetX + squareWidth || y < squareOffsetY || y > squareOffsetY + squareHeight)
+                        {
+                            if (!invert)
+                            {
+                                data[x, y] = true;
+                            }
+                            else
+                            {
+                                data[x, y] = false;
+                            }
+                        }
                     }
+
                 }
             }
 
