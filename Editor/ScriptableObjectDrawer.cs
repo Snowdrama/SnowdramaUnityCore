8aabc8eabc0ddec059ee93b4712853d21efb7f0a
John 'Snowdrama <SnowdramaGames@gmail.com>
2025-10-03 23:06:46 -0400

    Remove create button from scriptale object drawer

--- Editor/ScriptableObjectDrawer.cs
+++ Editor/ScriptableObjectDrawer.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEditor;
-using System.IO;
 
 [CustomPropertyDrawer(typeof(ScriptableObject), true)]
 public class ScriptableObjectDrawer : PropertyDrawer
@@ -25,92 +24,43 @@ public class ScriptableObjectDrawer : PropertyDrawer
 
         return totalHeight;
     }
+
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        // Reserve space for the foldout arrow (even if null)
-        Rect foldoutRect = new Rect(position.x, position.y, 0f, EditorGUIUtility.singleLineHeight);
-        Rect objectRect = new Rect(position.x + 0f, position.y, position.width - 0f, EditorGUIUtility.singleLineHeight);
+        // Draw object field
+        Rect objectRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
+        EditorGUI.BeginProperty(objectRect, label, property);
 
-        EditorGUI.BeginProperty(position, label, property);
-
-        // Foldout only makes sense if we have an object
-        if (property.objectReferenceValue != null)
-        {
-            foldout = EditorGUI.Foldout(foldoutRect, foldout, GUIContent.none, true);
-        }
-
-        // Draw the object field shifted over
-        property.objectReferenceValue = EditorGUI.ObjectField(
-            objectRect,
-            label,
-            property.objectReferenceValue,
-            fieldInfo.FieldType,
-            false
-        );
-
-        // Add "Create" button if null
-        if (property.objectReferenceValue == null)
-        {
-            Rect buttonRect = new Rect(objectRect.xMax - 60, objectRect.y, 60, EditorGUIUtility.singleLineHeight);
-            if (GUI.Button(buttonRect, "Create"))
-            {
-                ShowCreateMenu(property);
-            }
-        }
+        property.objectReferenceValue = EditorGUI.ObjectField(objectRect, label, property.objectReferenceValue, fieldInfo.FieldType, false);
 
         EditorGUI.EndProperty();
 
-        // Inline editor if expanded
-        if (foldout && property.objectReferenceValue != null)
+        // Draw foldout and inline inspector if assigned
+        if (property.objectReferenceValue != null)
         {
-            EditorGUI.indentLevel++;
-            SerializedObject serializedObject = new SerializedObject(property.objectReferenceValue);
-            SerializedProperty iterator = serializedObject.GetIterator();
+            Rect foldoutRect = new Rect(position.x + 15f, position.y, position.width, EditorGUIUtility.singleLineHeight);
+            foldout = EditorGUI.Foldout(foldoutRect, foldout, GUIContent.none, true);
 
-            float y = position.y + EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
-            iterator.NextVisible(true); // skip script
-            while (iterator.NextVisible(false))
+            if (foldout)
             {
-                float height = EditorGUI.GetPropertyHeight(iterator, true);
-                Rect propRect = new Rect(position.x, y, position.width, height);
+                EditorGUI.indentLevel++;
+                SerializedObject serializedObject = new SerializedObject(property.objectReferenceValue);
+                SerializedProperty iterator = serializedObject.GetIterator();
 
-                EditorGUI.PropertyField(propRect, iterator, true);
-                y += height + EditorGUIUtility.standardVerticalSpacing;
+                float y = position.y + EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+                iterator.NextVisible(true); // Skip script field
+                while (iterator.NextVisible(false))
+                {
+                    float height = EditorGUI.GetPropertyHeight(iterator, true);
+                    Rect propRect = new Rect(position.x, y, position.width, height);
+
+                    EditorGUI.PropertyField(propRect, iterator, true);
+                    y += height + EditorGUIUtility.standardVerticalSpacing;
+                }
+
+                serializedObject.ApplyModifiedProperties();
+                EditorGUI.indentLevel--;
             }
-
-            serializedObject.ApplyModifiedProperties();
-            EditorGUI.indentLevel--;
         }
-    }
-
-    private void ShowCreateMenu(SerializedProperty property)
-    {
-        GenericMenu menu = new GenericMenu();
-
-        menu.AddItem(new GUIContent("New " + fieldInfo.FieldType.Name), false, () =>
-        {
-            string path = "Assets";
-            if (Selection.activeObject != null)
-            {
-                string selectedPath = AssetDatabase.GetAssetPath(Selection.activeObject);
-                if (Directory.Exists(selectedPath))
-                    path = selectedPath;
-                else if (!string.IsNullOrEmpty(selectedPath))
-                    path = Path.GetDirectoryName(selectedPath);
-            }
-
-            string assetPath = AssetDatabase.GenerateUniqueAssetPath(Path.Combine(path, fieldInfo.FieldType.Name + ".asset"));
-            ScriptableObject asset = ScriptableObject.CreateInstance(fieldInfo.FieldType);
-
-            AssetDatabase.CreateAsset(asset, assetPath);
-            AssetDatabase.SaveAssets();
-            AssetDatabase.Refresh();
-
-            property.objectReferenceValue = asset;
-            property.serializedObject.ApplyModifiedProperties();
-            EditorGUIUtility.PingObject(asset);
-        });
-
-        menu.ShowAsContext();
     }
 }
