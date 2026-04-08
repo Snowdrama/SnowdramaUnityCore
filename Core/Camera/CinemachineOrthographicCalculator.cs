c8cd0303787c66b4f08737976614249b534a8caf
John 'Snowdrama <SnowdramaGames@gmail.com>
2026-04-07 22:00:25 -0400

    Fixed Ortho calc for VCams to make more sense

--- Core/Camera/CinemachineOrthographicCalculator.cs
+++ Core/Camera/CinemachineOrthographicCalculator.cs
@@ -6,59 +6,73 @@ using UnityEngine;
 [ExecuteAlways]
 public class CinemachineOrthographicCalculator : MonoBehaviour
 {
-    public float verticalUnits;
-    public float horizontalUnits;
+    //seems Default PPU is 256
+    public int pixelsPerUnit = 32;
+
+    //public Vector2 currentResolution = new Vector2(1920, 1080);
+    //The resolution that should be fit to the current resolution
+    public Vector2 targetResolution = new Vector2(512, 256);
+
+    //since the example target resolution is 256x256
+    //PPU fits inside the target resolution 16 times wide, and 8 times tall
+    public Vector2 orthographicScale = new Vector2(16, 8);
+
+    //because of this the actual ortho size is 
+    public float orthographicSize = 4.0f;
+
+    private int _pixelsPerUnitOld = 32;
+    private Vector2 _targetResolutionOld;
+
+    private Vector2 currentScreenSize;
 
     private CinemachineCamera cinemachineCamera;
-    private float width;
-    private float height;
-    private float orthoSize;
-    private Vector2 currentScreenSize;
-    private float verticalUnitsOld;
-    private float horizontalUnitsOld;
 
+    [SerializeField] private float ratio = 1.0f;
     private void Update()
     {
         if (cinemachineCamera == null)
         {
             cinemachineCamera = this.GetComponent<CinemachineCamera>();
+            return;
         }
-        if (currentScreenSize.x != Screen.width || currentScreenSize.y != Screen.height || verticalUnits != verticalUnitsOld || horizontalUnits != horizontalUnitsOld)
+
+        //was anything changed?
+        if (
+            currentScreenSize.x != Screen.width ||
+            currentScreenSize.y != Screen.height ||
+            targetResolution.x != _targetResolutionOld.x ||
+            targetResolution.y != _targetResolutionOld.y ||
+            pixelsPerUnit != _pixelsPerUnitOld
+            )
         {
-            verticalUnitsOld = verticalUnits;
-            horizontalUnitsOld = horizontalUnits;
-            Debug.LogWarningFormat("Old Resolution: [{0}, {1}]", currentScreenSize.x, currentScreenSize.y);
-            Debug.LogWarningFormat("New Resolution: [{0}, {1}]", Screen.width, Screen.height);
-            currentScreenSize.x = Screen.width;
-            currentScreenSize.y = Screen.height;
-            width = this.CalcWidth();
-            height = this.CalcHeight();
-            orthoSize = 5.0f;
-            if (width > height)
+            //update the old internal unit values
+            _targetResolutionOld = targetResolution;
+            currentScreenSize = new Vector2(Screen.width, Screen.height);
+
+            //600 / 800 * 0.5f;
+            ratio = (float)Screen.height / Screen.width * 0.5f;
+            orthographicScale = new Vector2(
+                targetResolution.x / pixelsPerUnit * Screen.height / Screen.width * 0.5f,
+                targetResolution.y / pixelsPerUnit * 0.5f
+            );
+
+            if (orthographicScale.x > orthographicScale.y)
             {
-                orthoSize = width;
+                orthographicSize = orthographicScale.x;
             }
             else
             {
-                orthoSize = height;
+                orthographicSize = orthographicScale.y;
             }
 
             cinemachineCamera.Lens.ModeOverride = LensSettings.OverrideModes.Orthographic;
-            cinemachineCamera.Lens.OrthographicSize = orthoSize;
+            cinemachineCamera.Lens.OrthographicSize = orthographicSize;
         }
     }
 
-    private float CalcWidth()
-    {
-        return horizontalUnits * Screen.height / Screen.width * 0.5f;
-    }
-    private float CalcHeight()
-    {
-        return verticalUnits * 0.5f;
-    }
-    private void OnDrawGizmosSelected()
+    private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
-        Gizmos.DrawWireCube(this.transform.position, new Vector3(horizontalUnits, verticalUnits, 0));
+        Gizmos.DrawWireCube(this.transform.position, new Vector3(orthographicScale.x * 1 / ratio, orthographicScale.y * 2, 0));
     }
 }
