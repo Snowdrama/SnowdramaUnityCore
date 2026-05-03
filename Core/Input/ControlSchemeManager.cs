8408dde70bd966c6f8892678cb1e68561a5f9cf9
John 'Snowdrama <SnowdramaGames@gmail.com>
2026-05-03 16:35:46 -0400

    Update to ControlManager

--- Core/Input/ControlSchemeManager.cs
+++ Core/Input/ControlSchemeManager.cs
@@ -1,5 +1,6 @@
 using System.Data;
 using UnityEngine;
+using UnityEngine.InputSystem;
 public enum ControlSchemeType
 {
     None,
@@ -26,5 +27,59 @@ public class ControlSchemeManager : MonoBehaviour
             SchemeType = type;
             changeMesage.Dispatch(type);
         }
+    }
+    [SerializeField] private InputActionReference[] mouseInputs;
+    [SerializeField] private InputActionReference[] gamepadInputs;
+    [SerializeField] private InputActionReference[] touchInputs;
+
+    private void OnEnable()
+    {
+        foreach (var input in mouseInputs)
+        {
+            input.action.performed += this.OnMousePerformed;
+            input.action.Enable();
+        }
+        foreach (var input in gamepadInputs)
+        {
+            input.action.performed += this.OnGamepadPerformed;
+            input.action.Enable();
+        }
+        foreach (var input in touchInputs)
+        {
+            input.action.performed += this.OnTouchPerformed;
+            input.action.Enable();
+        }
+
+    }
+    private void OnDisable()
+    {
+        foreach (var input in mouseInputs)
+        {
+            input.action.Disable();
+            input.action.performed -= this.OnMousePerformed;
+        }
+        foreach (var input in gamepadInputs)
+        {
+            input.action.Disable();
+            input.action.performed -= this.OnGamepadPerformed;
+        }
+        foreach (var input in touchInputs)
+        {
+            input.action.Disable();
+            input.action.performed -= this.OnTouchPerformed;
+        }
+    }
+
+    public void OnMousePerformed(InputAction.CallbackContext context)
+    {
+        RequestSchemeType(ControlSchemeType.KBM);
+    }
+    public void OnGamepadPerformed(InputAction.CallbackContext context)
+    {
+        RequestSchemeType(ControlSchemeType.Gamepad);
+    }
+    public void OnTouchPerformed(InputAction.CallbackContext context)
+    {
+        RequestSchemeType(ControlSchemeType.Touch);
     }
 }
