94bf2166484139e80b0819cc30ad23fcf660faec
John 'Snowdrama <SnowdramaGames@gmail.com>
2026-07-01 22:31:37 -0400

    Add textbox sizing to tooltip

--- Core/GameFeatures/Tooltips/TooltipDisplay.cs
+++ Core/GameFeatures/Tooltips/TooltipDisplay.cs
@@ -7,12 +7,14 @@ public class TooltipDisplay : MonoBehaviour
 {
     private TooltipData data;
     private RectTransform rectTransform;
+    private RectTransform tooltipTextTransform;
 
     [SerializeField] private TMP_Text tooltipText;
 
     private void Start()
     {
         rectTransform = this.GetComponent<RectTransform>();
+        tooltipTextTransform = tooltipText.GetComponent<RectTransform>();
     }
 
     private void Update()
@@ -44,6 +46,12 @@ public class TooltipDisplay : MonoBehaviour
         rectTransform.position = data.position;
         rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, data.size.x);
         rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, data.size.y);
+
+
+        tooltipTextTransform.anchorMin = new Vector2(0, 0);
+        tooltipTextTransform.anchorMax = new Vector2(1, 1);
+        tooltipTextTransform.offsetMin = new Vector2(10, 10);
+        tooltipTextTransform.offsetMax = new Vector2(10, 10);
     }
 
     private Action onRemove;
