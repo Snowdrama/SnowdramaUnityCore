d2272eb096fbe849f5b047f6014ebb09119028de
John 'Snowdrama <SnowdramaGames@gmail.com>
2026-07-18 01:14:01 -0400

    Added comments to physics tools because it's a bit funky to understand, should explain better

--- Core/Extensions/PhysicsTools.cs
+++ Core/Extensions/PhysicsTools.cs
@@ -8,26 +8,107 @@ public class PhysicsTools
     /// the player to jump and how long you want the player 
     /// to take 
     /// </summary>
-    /// <param name="fallHeight">The height of the jump in terms of
-    /// how far the player would fall from the peak of the jump</param>
-    /// <param name="timeToReachGround">how long it would take to go from 
-    /// the peak of the jump to the ground</param>
+    /// <param name="jumpHeight">
+    /// The height of the jump
+    /// 
+    /// how far the player would fall from the peak of the jump
+    /// </param>
+    /// <param name="timeToReachGround">
+    /// Half the time of the whole jump.
+    /// 
+    /// How long it would take to go from
+    /// the ground to the peak or from
+    /// the peak of the jump to the ground.
+    /// </param>
     /// <returns>A value for gravity</returns>
-    public static double DeriveGravity(double fallHeight, double timeToReachGround)
+    public static double DeriveGravity(double jumpHeight, double timeToReachGround)
     {
-        return 2 * fallHeight / (timeToReachGround * timeToReachGround);
+        return 2 * jumpHeight / (timeToReachGround * timeToReachGround);
     }
 
+
+    /// <summary>
+    /// Derives a value for Gravity based on how high you want 
+    /// the player to jump and how long you want the player 
+    /// to take 
+    /// 
+    /// Combined with PhysicsTools.DeriveInitialVelocity
+    /// This creates a parabola where you jump with some force from the ground
+    /// reach some 'jumpHeight' and then return to the ground in 'jumpTime'
+    /// </summary>
+    /// <param name="jumpHeight">
+    /// The height of the jump
+    /// 
+    /// In terms of the parabola of the player's height
+    /// this is the peak of the parabola
+    /// </param>
+    /// <param name="timeToReachPeak">
+    /// Half the time of the whole jump.
+    /// 
+    /// How long it would take to go from
+    /// the ground to the peak or from
+    /// the peak of the jump to the ground.
+    /// </param>
+    /// <returns>A value for gravity</returns>
+    public static float DeriveGravity(float jumpHeight, float timeToReachPeak)
+    {
+        return 2 * jumpHeight / (timeToReachPeak * timeToReachPeak);
+    }
+
+
+    /// <summary>
+    /// Derives the force needed to reach a certain height given some gravity and some time
+    /// </summary>
+    /// <param name="jumpHeight">
+    /// The height of the jump
+    /// 
+    /// In terms of the parabola of the player's height
+    /// this is the peak of the parabola
+    /// </param>
+    /// <param name="timeToReachPeak">
+    /// Half the time of the whole jump.
+    /// 
+    /// How long it would take to go from
+    /// the ground to the peak or from
+    /// the peak of the jump to the ground.
+    /// </param>
+    /// <param name="gravity">
+    /// How strong the influence of gravity is.
+    /// 
+    /// Use PhysicsTools.DeriveGravity to get a gravity 
+    /// from the jump height and timeToReachPeak that matches
+    /// the inputs for PhysicsTools.DeriveInitialVelocity
+    /// </param>
+    /// <returns>The velocity to use to match the parabola described by the jump parameters</returns>
     public static double DeriveInitialVelocity(double jumpHeight, double timeToReachPeak, double gravity = -9.8f)
     {
         return (jumpHeight - (0.5f * gravity * timeToReachPeak * timeToReachPeak)) / timeToReachPeak;
     }
 
-    public static float DeriveGravity(float fallHeight, float timeToReachGround)
-    {
-        return 2 * fallHeight / (timeToReachGround * timeToReachGround);
-    }
-
+    /// <summary>
+    /// Derives the force needed to reach a certain height given some gravity and some time
+    /// </summary>
+    /// <param name="jumpHeight">
+    /// The height of the jump
+    /// 
+    /// In terms of the parabola of the player's height
+    /// this is the peak of the parabola
+    /// </param>
+    /// <param name="timeToReachPeak">
+    /// Half the time of the whole jump.
+    /// 
+    /// How long it would take to go from
+    /// the ground to the peak or from
+    /// the peak of the jump to the ground.
+    /// </param>
+    /// <param name="gravity">
+    /// How strong the influence of gravity is.
+    /// 
+    /// Use PhysicsTools.DeriveGravity to get a gravity 
+    /// from the jump height and timeToReachPeak that matches
+    /// the inputs for PhysicsTools.DeriveInitialVelocity
+    /// </param>
+    /// <returns>The velocity to use to match the parabola described by the jump parameters</returns>
     public static float DeriveInitialVelocity(float jumpHeight, float timeToReachPeak, float gravity = -9.8f)
     {
         return (jumpHeight - (0.5f * gravity * timeToReachPeak * timeToReachPeak)) / timeToReachPeak;
