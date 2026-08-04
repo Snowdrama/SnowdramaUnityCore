278399932a625a62c2362c6ae311c35f17d99156
John 'Snowdrama <SnowdramaGames@gmail.com>
2026-08-04 05:51:30 -0400

    Added some Vector2Extension stuff

--- Core/Extensions/Vector2Extensions.cs
+++ Core/Extensions/Vector2Extensions.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.SocialPlatforms;
 public static class Vector2Extensions
 {
+    private static System.Random rand = new System.Random();
     public static Vector2 VectorFromAngleRads(float angle)
     {
         var V = new Vector2();
@@ -60,6 +61,11 @@ public static class Vector2Extensions
             angle += 360;
         }
         return angle;
+    }
+
+    public static float AngleFromVectorDegrees(this Vector2 dir)
+    {
+        return Mathf.Rad2Deg * dir.AngleFromVectorRads();
     }
 
     public static float AngleFromVectorRads(this Vector2 dir)
@@ -153,6 +159,35 @@ public static class Vector2Extensions
     }
 
     /// <summary>
+    /// Given a size, and a targetSize, finds the scale factor needed to 
+    /// fit the size into the target size, Including fractional proportions 
+    /// when the size would need to be decreased to fit the size
+    /// 
+    /// For example if I have a size of(2, 2) and a target of(16, 9), it 
+    /// would return a scale factor of(4.5, 4.5) as `(2 * 4.5, 2 * 4.5)` 
+    /// would be(9, 9) and is the smallest that would fit inside(16, 9). 
+    /// 
+    /// If the `stretchToFit` option is enabled, this will return a non 
+    /// uniform scale.For example finding the scale factor of (2, 2) to 
+    /// (16, 9) would in this case be(8, 4.5) as multiplying `(2 * 8, 2 * 4.5)`
+    /// would give you the target scale of(16, 9). 
+    /// 
+    /// </summary>
+    /// <param name="size"></param>
+    /// <param name="targetSize"></param>
+    /// <param name="stretchToFit"></param>
+    /// <returns></returns>
+    public static Vector2 FindScaleFactorIntegerScaled(this Vector2 size, Vector2 targetSize)
+    {
+        //we're trying to find the scale factor that will fit the 
+        var fullScale = targetSize / size;
+        var intSize = Mathf.FloorToInt(Mathf.Min(fullScale.x, fullScale.y));
+
+        return new Vector2(intSize, intSize);
+    }
+
+
+    /// <summary>
     /// calculates the aspect ratio from the resolution
     /// </summary>
     /// <param name="size"></param>
@@ -173,9 +208,13 @@ public static class Vector2Extensions
     }
 
 
+    /// <summary>
+    /// Get a random direction
+    /// </summary>
+    /// <returns></returns>
     public static Vector2 RandomDirection()
     {
-        return Vector2Extensions.VectorFromAngle((float)UnityEngine.Random.Range(0, Mathf.PI));
+        return Vector2Extensions.VectorFromAngle((float)UnityEngine.Random.Range(0, 360));
     }
 
     public static float AsRandomRange(this Vector2 range)
@@ -196,5 +235,174 @@ public static class Vector2Extensions
         }
 
         return value;
+    }
+    public static Vector2 RoundToGrid(this Vector2 pos, Vector2 gridSize, Vector2 offset = new Vector2())
+    {
+        return new Vector2(
+            (Mathf.Round((pos.x + offset.x) / gridSize.x) * gridSize.x) - offset.x,
+            (Mathf.Round((pos.y + offset.y) / gridSize.y) * gridSize.y) - offset.x);
+    }
+    public static Vector2Int RoundToGridCell(this Vector2 pos, Vector2 gridSize, Vector2Int offset = new Vector2Int())
+    {
+        return new Vector2Int(
+            Mathf.RoundToInt((pos.x + offset.x) / gridSize.x) - offset.x,
+            Mathf.RoundToInt((pos.y + offset.y) / gridSize.y) - offset.x);
+    }
+    public static Vector2 FloorToGrid(this Vector2 pos, Vector2 gridSize, Vector2 offset = new Vector2())
+    {
+        return new Vector2(
+            (Mathf.Floor((pos.x + offset.x) / gridSize.x) * gridSize.x) - offset.x,
+            (Mathf.Floor((pos.y + offset.y) / gridSize.y) * gridSize.y) - offset.x);
+    }
+
+    public static Vector2Int FloorToGridCell(this Vector2 pos, Vector2 gridSize, Vector2Int offset = new Vector2Int())
+    {
+        return new Vector2Int(
+            Mathf.FloorToInt((pos.x + offset.x) / gridSize.x) - offset.x,
+            Mathf.FloorToInt((pos.y + offset.y) / gridSize.y) - offset.x);
+    }
+    public static Vector2 CeilToGrid(this Vector2 pos, Vector2 gridSize, Vector2 offset = new Vector2())
+    {
+        return new Vector2(
+            (Mathf.Ceil((pos.x + offset.x) / gridSize.x) * gridSize.x) - offset.x,
+            (Mathf.Ceil((pos.y + offset.y) / gridSize.y) * gridSize.y) - offset.x);
+    }
+    public static Vector2Int CeilToGridCell(this Vector2 pos, Vector2 gridSize, Vector2Int offset = new Vector2Int())
+    {
+        return new Vector2Int(
+            Mathf.CeilToInt((pos.x + offset.x) / gridSize.x) - offset.x,
+            Mathf.CeilToInt((pos.y + offset.y) / gridSize.y) - offset.x);
+    }
+
+    public static Vector2 RadialClamp(this Vector2 pos, Vector2 center, float range)
+    {
+        var dir = pos - center;
+        var len = dir.magnitude;
+        if (len > range)
+        {
+            return center + (dir.normalized * range);
+        }
+        return pos;
+    }
+
+
+    public static Vector2Int Clamp(this Vector2Int val, Vector2Int min, Vector2Int max)
+    {
+        return new Vector2Int(Mathf.Clamp(val.x, min.x, max.x), Mathf.Clamp(val.y, min.y, max.y));
+    }
+
+    /// <summary>
+    /// Given a Vector2, return a float between the X and Y value
+    /// </summary>
+    /// <param name="val"></param>
+    /// <returns></returns>
+    public static float RandomBetweenXY(this Vector2 val)
+    {
+        return RandomAndNoise.RandomRange(val.x, val.y);
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="val">The current position</param>
+    /// <param name="magnitude">the distance from the original position that you want the point</param>
+    /// <returns>A new position some distance away from the original position</returns>
+    public static Vector2 RandomDirectionOffset(this Vector2 val, float magnitude = 1.0f)
+    {
+        return val + (RandomDirection() * magnitude);
+    }
+
+
+    public static Vector2 Random(float minX, float maxX, float minY, float maxY, bool normalized = true)
+    {
+        if (normalized)
+        {
+            return new Vector2((float)Mathf.Lerp(minX, maxX, (float)rand.NextDouble()), (float)Mathf.Lerp(minY, maxY, (float)rand.NextDouble())).normalized;
+        }
+        return new Vector2((float)Mathf.Lerp(minX, maxX, (float)rand.NextDouble()), (float)Mathf.Lerp(minY, maxY, (float)rand.NextDouble()));
+    }
+
+    /// <summary>
+    /// Gets a point that is the combination of the smallest value of both pointArr 
+    /// 
+    /// For example if you have the pointArr (3,5) and (2,9) the result would be (2, 5)
+    /// </summary>
+    /// <param name="A"></param>
+    /// <param name="B"></param>
+    /// <returns>a new vector that has the smallest value of both pointArr</returns>
+    public static Vector2 Min(Vector2 A, Vector2 B)
+    {
+        return new Vector2(Mathf.Min(A.x, B.x), Mathf.Min(A.y, B.y));
+    }
+
+    /// <summary>
+    /// Gets a point that is the combination of the largest value of both pointArr 
+    /// 
+    /// For example if you have the pointArr (3,5) and (2,9) the result would be (3, 9)
+    /// </summary>
+    /// <param name="A"></param>
+    /// <param name="B"></param>
+    /// <returns>a new vector that has the largest value of both pointArr</returns>
+    public static Vector2 Max(Vector2 A, Vector2 B)
+    {
+        return new Vector2(Mathf.Max(A.x, B.x), Mathf.Max(A.y, B.y));
+    }
+
+
+    public static bool InBounds(this Vector2 pos, Vector2 TopLeftPos, Vector2 BottomRightPos)
+    {
+        if (pos.x >= TopLeftPos.x &&
+            pos.x < BottomRightPos.x &&
+            pos.y >= TopLeftPos.y &&
+            pos.y < BottomRightPos.y)
+        {
+            return true;
+        }
+        return false;
+    }
+    public static bool InBounds(this Vector2Int pos, Vector2Int TopLeftPos, Vector2Int BottomRightPos)
+    {
+        if (pos.x >= TopLeftPos.x &&
+            pos.x < BottomRightPos.x &&
+            pos.y >= TopLeftPos.y &&
+            pos.y < BottomRightPos.y)
+        {
+            return true;
+        }
+        return false;
+    }
+    public static bool InBounds(this Vector2Int pos, Vector2 TopLeftPos, Vector2 BottomRightPos)
+    {
+        return InBounds(pos, TopLeftPos.RoundToInt(), BottomRightPos.RoundToInt());
+    }
+
+    public static Vector2 Lerp(Vector2 start, Vector2 end, float t)
+    {
+        return new Vector2(Mathf.Lerp(start.x, end.x, t), Mathf.Lerp(start.y, end.y, t));
+    }
+
+    public static Vector2 MoveTowards(this Vector2 from, Vector2 to, float delta)
+    {
+        var translation = to - from;
+
+        var scale = Mathf.Min(translation.magnitude, delta);
+
+        var newVector = from + (translation.normalized * scale);
+
+        return newVector;
+    }
+
+    public static int IndexFromPosition(this Vector2Int position, Vector2Int size)
+    {
+        return position.x + (position.y * size.x);
+    }
+
+    public static Vector2Int PositionFromIndex(this int position, Vector2Int size)
+    {
+        var y = Mathf.FloorToInt(position / size.x);
+        var x = position - (y * size.x);
+
+        return new Vector2Int(x, y);
     }
 }
